7307c43d85035a17e948edb053a040979348f919
juan <jcintasr01@iesalbarregas.es>
2026-03-23 10:20:14 +0100

    selector por seguidos.java realizado

--- Algoritmos/SelectorPorSeguidos.java
+++ Algoritmos/SelectorPorSeguidos.java
@@ -1,5 +1,29 @@
 package Algoritmos;
 
-public class SelectorPorSeguidos {
-    
+import java.util.*;
+
+/**
+ * Algoritmo 3: Prioriza publicaciones de usuarios que el usuario actual sigue.
+ */
+public class SelectorPorSeguidos implements SelectorContenido {
+
+    @Override
+    public List<Contenido> seleccionar(List<Contenido> contenidos, Usuario usuario) {
+        List<Contenido> seguidosPrimero = new ArrayList<>();
+        List<Contenido> resto = new ArrayList<>();
+
+        Set<Usuario> usuariosSeguidos = usuario.getSeguidos();
+
+        for (Contenido contenido : contenidos) {
+            if (usuariosSeguidos.contains(contenido.getAutor())) {
+                seguidosPrimero.add(contenido);
+            } else {
+                resto.add(contenido);
+            }
+        }
+
+        // Combinar: seguidos primero, luego resto
+        seguidosPrimero.addAll(resto);
+        return seguidosPrimero;
+    }
 }
